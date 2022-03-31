e2490b90d260e227fa6e5c957e363f825705b012
Andres <andresardila@me.com>
2022-03-31 11:48:02 -0400

    Add project files.

---
+++ API_Album/API_Album/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using API_Album.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace API_Album
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            fetchAlbum(); 
+        }
+
+        public async void fetchAlbum()
+        {
+            string url = "https://jsonplaceholder.typicode.com/photos";
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var albums = JsonConvert.DeserializeObject<List<Album>>(response);
+                carouselItems.ItemsSource = albums;
+
+            }
+        }
+
+
+    }
+}
---
+++ API_Album/API_Album/Model/Album.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Album.Model
+{
+    class Album
+    {
+        [JsonProperty("albumId")]
+        public int AlbumId { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("thumbnailUrl")]
+        public string ThumbnailUrl { get; set; }
+
+        public string tbUrl { get { return this.ThumbnailUrl + ".png"; } }
+    }
+}
